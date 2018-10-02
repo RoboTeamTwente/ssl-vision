07f480cb0320d9825776ee6e6232ec48b8b6a76e
ThijsLuttikhuis <t.luttikhuis@student.utwente.nl>
2018-10-02 17:10:46 +0200

    added new detection script, replacing the aruco library. testing and tweaking needed

---
+++ src/shared/aruco/aruco_markerfinder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by thijs on 2-10-18.
+//
+
+#include "aruco_markerfinder.h"
---
+++ src/shared/aruco/aruco_markerfinder.h
@@ -0,0 +1,14 @@
+//
+// Created by thijs on 2-10-18.
+//
+
+#ifndef SSL_VISION_ARUCO_MARKERFINDER_H
+#define SSL_VISION_ARUCO_MARKERFINDER_H
+
+
+class aruco_markerfinder {
+
+};
+
+
+#endif //SSL_VISION_ARUCO_MARKERFINDER_H
